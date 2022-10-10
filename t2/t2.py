c8976434b1df01ac9e1455653353799fe3315fb6
Randerson Lemos <randerson.lemos@gmail.com>
2022-10-10 17:16:28 -0300

    Merge sort implemented

--- t2/t2.py
+++ t2/t2.py
@@ -4,30 +4,35 @@ def swap(X, i, j):
     X[j] = keep
 
 
+
+
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file', type=argparse.FileType('r'))
-    args = parser.parse_args()
-    n = int( args.file.readline().strip() )
-    X = list( map(int, args.file.readline().strip().split(' ') ) )
+    #import argparse
+    #import time
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('--file', type=argparse.FileType('r'))
+    #args = parser.parse_args()
+    #n = int( args.file.readline().strip() )
+    #X = list( map(int, args.file.readline().strip().split(' ') ) )
 
-    #n = int( input().strip() )
-    #X = list( map(int, input().strip().split(' ')) )
-
-    #print(X)
+    n = int( input().strip() )
+    X = list( map(int, input().strip().split(' ')) )
 
     count = 0
-    IMIN = 0
-    IMAX = n - 1
+    IMIN  = 0
+    IMAX  = n - 1
 
+    #print('n:',n)
+
+    #t = time.time()
     while True:
         for i in range(IMIN, IMAX):
             j = i + 1
             if X[i] > X[j]:
-                swap(X,i,j)
+                keep = X[i]
+                X[i] = X[j]
+                X[j] = keep
                 count += 1
-                no_swap = False
 
         if (IMIN + 1) == X[IMIN]:
             IMIN += 1
@@ -38,8 +43,8 @@ if __name__ == '__main__':
         if IMIN > IMAX:
             break
 
-
-    #print(X)
+    #print('Time {:3.6f}'.format(time.time() - t))
+    #print('n^2', n*n)
     print(count)
 
 
