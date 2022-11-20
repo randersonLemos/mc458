4edd992c851df9ed0d68531ce7251c520206c122
Randerson Lemos <randerson.lemos@gmail.com>
2022-11-19 23:33:05 -0300

    Work 4 done

--- t4/t4.py
+++ t4/t4.py
@@ -15,38 +15,36 @@ def print_matrix(m):
     print('\n'.join(['{}'.format(['{:5.0f}'.format(el) for el in row]).strip(']').strip('[') for row in shell]).replace("'", '').replace(',', ''))
 
 
-
-
-def PD(n, notas, V):
-    _V = V
-    V += max(notas)
-
-    r = np.zeros((n+1, V+1)).astype(float)
+def PD(n, notas, V, U):
+    r = np.empty((n+1, V+1)).astype(float)
     r[0, :] = np.inf
     r[:, 0] = 0
-
 
     #print(notas, V)
     #print_matrix(r)
     #print('======')
 
-    for i in range(1, n+1):
-        for v in range(1, V+1):
+    for v in range(1, V+1):
+        for i in range(1, n+1):
             nota = notas[i-1]
             if nota > v:
                 r[i, v] = r[i-1,v]
             else:
-                div = v//nota
-                aux = r[i-1,v] 
-                for d in range(1,div+1):
-                    if aux > r[i-1, v - nota*d] + d:
-                        aux = r[i-1, v - nota*d] + d
-                r[i,v] = aux
+                #div = v//nota
+                #aux = r[i-1,v] 
+                #for d in range(1,div+1):
+                #    if aux > r[i-1, v - nota*d] + d:
+                #        aux = r[i-1, v - nota*d] + d
+                #r[i,v] = aux
+                r[i,v] = min(r[i-1,v], r[i, v-nota] + 1, r[i-1,v-nota] + 1)
             
-            if v >= V - max(notas) and i == n and r[i,v] != np.inf:
-                #print_matrix(r[:i+1,:v+1])
-                #print('======')
+            if v >= U and i == n and r[i,v] != np.inf:
                 return v, int(r[i,v])
+
+
+        #print_matrix(r[:,:v+1])
+        #print('======')
+
 
 
 if __name__ == '__main__':
@@ -54,15 +52,23 @@ if __name__ == '__main__':
     #parser = argparse.ArgumentParser()
     #parser.add_argument('--file', required=True, type=argparse.FileType('r'))
     #args = parser.parse_args()
-    #V = int( args.file.readline().strip() )
+    #U = int( args.file.readline().strip() )
     #n = int( args.file.readline().strip() )
     #notas = list( map( int, args.file.readline().strip().split(' ') ) )
 
-    V = int( input().strip() )
+    U = int( input().strip() )
     n = int( input().strip() )
     notas = list( map(int, input().strip('\n').split(' ')) )
 
-    V, n = PD(n, notas, V)
+    #aux = np.inf
+    #print('-'.join( map(str,notas) ) , '|' ,V) 
+    #for nota in notas:
+    #    mod = V%nota
+    #    print(mod)
+
+
+    V = U + max(notas)
+    V, n = PD(n, notas, V, U)
 
     print(V,n)
 
